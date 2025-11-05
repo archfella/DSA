5c8d50e465da4eea1fc66a4cd934948d24f5e696
Filip Radovic <filipradovic@Filips-MacBook-Pro.local>
2025-11-05 22:23:18 +0100

    Day 3: First not divisible - invariant v2

--- algorithms/first_not_divisible.cpp
+++ algorithms/first_not_divisible.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 
 /*
-    Invariant:
+    V1_Invariant:
         - Elements [0, left) don't satisfy COND.
         - Elements (right, a.size()) satisfy COND.
         - Elements [left, right] are not yet evaluated.
@@ -29,6 +29,17 @@ int first_not_divisible_V1(const std::vector<long long> &a, long long k)
 
     return left; // !
 }
+
+/*
+    V2_Invariant:
+        - Elements [0, left) don't satisfy COND.    !!! DIFFERENT !!!
+        - Elements [right, a.size()) satisfy COND.  !!! DIFFERENT !!!
+        - Elements [left, right) are not yet evaluated. !!! DIFFERENT !!!
+        * COND: Element is not divisible by 'k'.
+
+        Result: Elements that satisfy COND are [right, a.size()), so the first element that
+        satisfies the COND is 'right'.
+*/
 
 int first_not_divisible_V2(const std::vector<long long> &a, long long k)
 {
