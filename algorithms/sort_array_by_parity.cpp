7a97595dbc44dfcdefc0ddabbfba75d2970306f6
Filip Radovic <filipradovic@Filips-MacBook-Pro.local>
2025-11-06 23:01:51 +0100

    sort aray by parity V2

--- algorithms/sort_array_by_parity.cpp
+++ algorithms/sort_array_by_parity.cpp
@@ -6,7 +6,7 @@
         - Elements (right, n) are odd.
         - Elements [left, right] are not yet evaluated.
 */
-void sort_array_by_parity(std::vector<int> &arr)
+void sort_array_by_parity_V1(std::vector<int> &arr)
 {
     int n = arr.size();
     int left = 0, right = n - 1;
@@ -17,6 +17,29 @@ void sort_array_by_parity(std::vector<int> &arr)
         {
             std::swap(arr[left], arr[right]);
             right--;
+        }
+        else
+            left++;
+    }
+}
+
+/*
+    Invariant:
+        - Elements (0, left) are even.
+        - Elements [right, n) are odd.
+        - Elements [left, right) are not yet evaluated.
+*/
+void sort_array_by_parity_V2(std::vector<int> &arr)
+{
+    int n = arr.size();
+    int left = 0, right = n;
+
+    while (left < right)
+    {
+        if (arr[left] % 2 == 1)
+        {
+            right--;
+            std::swap(arr[left], arr[right]);
         }
         else
             left++;
